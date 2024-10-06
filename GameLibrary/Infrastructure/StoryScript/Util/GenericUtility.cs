8a075769b2cc967172bcf5518822254131772ce6
dreamanlan <dreamanlan@tencent.com>
2024-10-06 15:12:22 +0800

    Update GenericUtility.cs

--- GameLibrary/Infrastructure/StoryScript/Util/GenericUtility.cs
+++ GameLibrary/Infrastructure/StoryScript/Util/GenericUtility.cs
@@ -2696,6 +2696,19 @@ namespace StoryScript
             bv.Set(v);
             return bv;
         }
+        //Generic From
+        public static BoxedValue From<T>(T v)
+        {
+            BoxedValue bv = new BoxedValue();
+            bv.GenericSet(v);
+            return bv;
+        }
+        public static BoxedValue From(Type t, object o)
+        {
+            BoxedValue bv = new BoxedValue();
+            bv.GenericSet(t, o);
+            return bv;
+        }
         //Used by Lua or to prevent implicit conversion problems
         public static BoxedValue FromBool(bool v)
         {
